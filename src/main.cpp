6455057c0ced45fc49b824ac18e820b265759294
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-03-10 07:00:49 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 #include <sstream>
 #include <string>
 #include <limits>
-#include <cstring>
-#include <HardwareSerial.h>
 
 RF24 radio(4, 5);           // nRF24L01(+) radio attached using Getting Started board
 RF24Network network(radio); // Network uses that radio
@@ -94,19 +92,19 @@ ParseData parse_msg(const std::string &input)
     std::string token;
 
     if (getline(iss, token, ','))
-        _temp.node_id = strtof(token.c_str(), NULL);
+        _temp.node_id = strtod(token.c_str(), NULL);
 
     // Parse voltage
     if (getline(iss, token, ','))
-        _temp.voltage = strtof(token.c_str(), NULL);
+        _temp.voltage = strtod(token.c_str(), NULL);
 
     // Parse current
     if (getline(iss, token, ','))
-        _temp.current = strtof(token.c_str(), NULL);
+        _temp.current = strtod(token.c_str(), NULL);
 
     // Parse power
     if (getline(iss, token, ','))
-        _temp.power = strtof(token.c_str(), NULL);
+        _temp.power = strtod(token.c_str(), NULL);
 
     // Parse switch state
     if (getline(iss, token, ','))
@@ -190,9 +188,6 @@ void bacaDO()
     fcelcius = dumTemp[random(0, 7)];
     fdopersen = dumDoa[random(0, 7)];
     fdomg = dumDob[random(0, 7)];
-    Serial.printf("\n Suhu : %f Celcius \n", fcelcius);
-    Serial.printf("\n Persentase DO : %f % \n", fdopersen);
-    Serial.printf("\n dum mg/L DO : %f % \n", fdomg);
 }
 
 void kontrol()
@@ -262,30 +257,17 @@ void kontrol()
     payload.ms = millis();
     payload.counter = packets_sent++;
 
-    for (size_t i = 0; i < 4; i++)
-    {
-        Serial.print(i);
-        Serial.print(" : ");
-        Serial.println(_temp_node[i]);
-    }
-
     for (int i = 0; i < 4; i++)
     {
         RF24NetworkHeader header(i + 1);
         payload.status = _temp_node[i];
         bool ok = network.write(header, &payload, sizeof(payload));
+        Serial.print("To node ");
+        Serial.print(i + 1);
         if (ok)
-        {
-            Serial.print("to node ");
-            Serial.print(i + 1);
-            Serial.println(": ok.");
-        }
+            Serial.println(": Ok.");
         else
-        {
-            Serial.print("to node ");
-            Serial.print(i + 1);
-            Serial.println(": failed.");
-        }
+            Serial.println(": Failed.");
     }
 }
 
@@ -332,145 +314,4 @@ void loop()
         bacaDO();
         kontrol();
     }
-}
-
-// void mintaData() {
-//   network.update();  // Check the network regularly
-//   unsigned long now = millis();
-//
-//   // Jika sudah waktunya mengirim pesan, kirim pesan ke semua node yang ditentukan
-//   if (now - last_sent >= interval1) {
-//     last_sent = now;
-//
-//     Serial.print(F("Sending... "));
-//     const char message[] = "R";
-//     payload_t payload;
-//     payload.ms = millis(); // Timestamp
-//     payload.counter = packets_sent++; // Counter
-//     strcpy(payload.message, message); // Copy message into payload
-//
-//     // Mengatur nodeID yang dituju
-//     payload.node_ID = node1 + (packets_sent - 1) % 4; // Bergantian antara node1 hingga node4
-//
-//     // Mengirim pesan ke node yang ditentukan
-//     RF24NetworkHeader header(payload.node_ID);
-//     bool ok = network.write(header, &payload, sizeof(payload));
-//     if (ok) {
-//       Serial.print(F("to node "));
-//       Serial.print(payload.node_ID);
-//       Serial.println(F(": ok."));
-//     } else {
-//       Serial.print(F("to node "));
-//       Serial.print(payload.node_ID);
-//       Serial.println(F(": failed."));
-//     }
-//   }
-// }
-
-// void mintaData() {
-//   network.update();  // Check the network regularly
-//   unsigned long now = millis();
-//
-//   // Jika sudah waktunya mengirim pesan, kirim pesan ke semua node yang ditentukan
-//   if (now - last_sent >= interval1) {
-//     last_sent = now;
-//
-//     Serial.print(F("Sending... "));
-//     const char message[] = "R";
-//     payload_t payload;
-//     payload.ms = millis(); // Timestamp
-//     payload.counter = packets_sent++; // Counter
-//     strcpy(payload.message, message); // Copy message into payload
-//
-//     // Mengatur nodeID yang dituju
-//     payload.nodeID = node1;
-//
-//     // Mengirim pesan ke semua node yang ditentukan
-//     for (uint16_t nodeID = node1; nodeID <= node4; ++nodeID) {
-//       RF24NetworkHeader header(nodeID);
-//       bool ok = network.write(header, &payload, sizeof(payload));
-//       if (ok) {
-//         Serial.print(F("to node "));
-//         Serial.print(nodeID);
-//         Serial.println(F(": ok."));
-//       } else {
-//         Serial.print(F("to node "));
-//         Serial.print(nodeID);
-//         Serial.println(F(": failed."));
-//       }
-//       delay(100); // Delay untuk memastikan pengiriman pesan selesai
-//     }
-//   }
-// }
-
-// void kontrol(){
-//   DOStatus _do_status;
-//   if (fdomg <= 4.f)
-//     _do_status = Rendah;
-//   else if (4.f < fdomg && fdomg <=6.f)
-//     _do_status = Normal;
-//   else
-//     _do_status = Tinggi;
-//
-//   char _temp_node[4] = {'0', '0', '0', '0'};
-//
-//
-////  Serial.println("Nilai DO:" + String(fdomg));
-//
-//  if (_do_status == Rendah){
-//    Serial.println("DO Rendah");
-//    char new_values[4] = {'1', '1', '1', '1'};
-//    std::strcpy(_temp_node, new_values);
-//    payload_t payload;
-//
-//  }
-//
-//  else if(_do_status == Normal)
-//  {
-//    int _target_node_id = 0;
-//    float _largest_current = -.1f;
-//    // Search highest current
-//    for(int i = 0; i < 4; i++)
-//      if(node_data[i].current > _largest_current)
-//      {
-//        _largest_current = node_data[i].current;
-//        _target_node_id = i;
-//      }
-//
-//    for(int i = 0; i < 4; i++)
-//      _temp_node[i] = _target_node_id == i ? '0' : '1';
-//  }
-//  else{
-//    int _target_node_id_1 = 0, _target_node_id_2 = 0;
-//    float _largest_current_1 = -.1f, _largest_current_2 = -.1;
-//    // Search highest current
-//    for(int i = 0; i < 4; i++)
-//    {
-//      if(node_data[i].current > _largest_current_1)
-//      {
-//        _largest_current_2 = _largest_current_1;
-//        _largest_current_1 = node_data[i].current;
-//        _target_node_id_2 = _target_node_id_1;
-//        _target_node_id_1 = i;
-//      }
-//      else if(node_data[i].current > _largest_current_2)
-//      {
-//        _largest_current_2 = node_data[i].current;
-//        _target_node_id_2 = i;
-//      }
-//    }
-//
-//    for(int i = 0; i < 4; i++)
-//      _temp_node[i] = _target_node_id_1 == i || _target_node_id_2 == i  ? '0' : '1';
-//  }
-//
-//  for(int i = 0; i < 4; i++) {
-//    Serial.print(_temp_node[i]);
-//    Serial.print('/');
-//  }
-//  Serial.println();
-//}
-
-void kirimExtender()
-{
 }
